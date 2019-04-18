5aef6766cf6fa019d2159cb91129eed80e7badb8
nexthis <36706292+pawcioo@users.noreply.github.com>
2019-04-18 13:16:23 +0200

    dodanie zarysu aplikacji

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,58 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(MyApp());
+import './screens/profile.dart';
+import './screens/quest.dart';
+import './screens/map.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(new MyApp());
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+class MyAppState extends State<MyApp> {
+  int _selectedTab = 1;
+  final _pageOptions = [
+      QuestTest(),
+      MapTest(),
+      ProfileTest(),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Google Maps Demo',
-      home: MapSample(),
-    );
-  }
-}
-
-class MapSample extends StatefulWidget {
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      theme: ThemeData(
+          primaryColor: Colors.teal[700],
+          primaryTextTheme: TextTheme(
+            title: TextStyle(color: Colors.white),
+          )),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Quaestio'),
+          ),
+        body: _pageOptions[_selectedTab],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedTab,
+          onTap: (int index) {
+            setState(() {
+              _selectedTab = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.queue),
+              title: Text('Zadania'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              title: Text('Mapa'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              title: Text('Konto'),
+            ),
+          ],
+        ),
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
-}
+  }}
---
+++ lib/screens/quest.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QuestTest extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("test1"),
+      ),
+    );
+  }
+}
